065464aff71f30845bfe44c6d081ab67553dd9e2
w4daka <213398204+w4daka@users.noreply.github.com>
2026-07-17 15:39:49 +0900

    feat(abc086/a.cpp)add

---
+++ abc086/a.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+  int a,b;
+  cin>> a >> b;
+  if ((a*b) %2 !=0) {
+   cout << "Odd" << endl;
+  }
+  else {
+   cout << "Even" << endl;
+  }
+}
+
