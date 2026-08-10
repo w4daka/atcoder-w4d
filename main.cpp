97bd148a1c8984348e9aeb951a171fcfe475eb49
w4daka <213398204+w4daka@users.noreply.github.com>
2026-08-10 15:00:12 +0900

    abc180 やった。

--- main.cpp
+++ main.cpp
@@ -4,12 +4,8 @@ using namespace std;
 
 
 int main() {
-  int h,a;
-  cin>> h >> a;
-  if (h % a == 0){
-    cout << h/a << endl;
-  }else{
-  cout << h / a + 1 << endl;
-  }
+  int n,a,b;
+  cin>> n >> a >> b ;
+  cout << n - a + b << endl;
 }
 
